a0e31ce760364e79a2ce7e79a83ee00a8f64dff5
ArushKumar <arushkumar023@gmail.com>
2025-06-10 15:16:39 +0530

    Btech JEE Validation

---
+++ Admission_Panel/src/in/ac/dei/edrp/admissionsystem/computation/ccaBean.java
@@ -0,0 +1,234 @@
+package in.ac.dei.edrp.admissionsystem.computation ;
+
+
+public class ccaBean 
+{
+
+	private String program_id;
+	private String app_number;
+	private String cca;
+	private String interview;
+	private String gd;
+	private String count_update;
+	private String count;
+	private String entity_id;
+	private String registration_number;
+	private String CA;
+	private String PW;
+	private String category;
+	private String evaluation_component;
+	private String application_number;
+	private String flag;
+	private String ProgramName;
+	private String class1;
+	private String marksObtained;
+    private String marks;
+    
+    private String seats;
+    private String newSeats;
+    private String gn;
+    private String bc;
+    private String sc;
+    private String st;
+    
+    
+	public String getProgram_id() {
+		return program_id;
+	}
+
+	public void setProgram_id(String programId) {
+		program_id = programId;
+	}
+
+	public String getApp_number() {
+		return app_number;
+	}
+
+	public void setApp_number(String appNumber) {
+		app_number = appNumber;
+	}
+
+	public String getCca() {
+		return cca;
+	}
+
+	public void setCca(String cca) {
+		this.cca = cca;
+	}
+
+	public String getInterview() {
+		return interview;
+	}
+
+	public void setInterview(String interview) {
+		this.interview = interview;
+	}
+
+	public String getCount_update() {
+		return count_update;
+	}
+
+	public void setCount_update(String countUpdate) {
+		count_update = countUpdate;
+	}
+
+	public String getEntity_id() {
+		return entity_id;
+	}
+
+	public void setEntity_id(String entityId) {
+		entity_id = entityId;
+	}
+
+	public String getRegistration_number() {
+		return registration_number;
+	}
+
+	public void setRegistration_number(String registrationNumber) {
+		registration_number = registrationNumber;
+	}
+
+	public String getCA() {
+		return CA;
+	}
+
+	public void setCA(String cA) {
+		CA = cA;
+	}
+
+	public String getPW() {
+		return PW;
+	}
+
+	public void setPW(String pW) {
+		PW = pW;
+	}
+
+	public String getEvaluation_component() {
+		return evaluation_component;
+	}
+
+	public void setEvaluation_component(String evaluationComponent) {
+		evaluation_component = evaluationComponent;
+	}
+
+	public String getApplication_number() {
+		return application_number;
+	}
+
+	public void setApplication_number(String applicationNumber) {
+		application_number = applicationNumber;
+	}
+
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	public String getProgramName() {
+		return ProgramName;
+	}
+
+	public void setProgramName(String programName) {
+		ProgramName = programName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getMarks() {
+		return marks;
+	}
+
+	public void setMarks(String marks) {
+		this.marks = marks;
+	}
+
+	public String getClass1() {
+		return class1;
+	}
+
+	public void setClass1(String class1) {
+		this.class1 = class1;
+	}
+
+	public String getMarksObtained() {
+		return marksObtained;
+	}
+
+	public void setMarksObtained(String marksObtained) {
+		this.marksObtained = marksObtained;
+	}
+
+	public String getSeats() {
+		return seats;
+	}
+
+	public void setSeats(String seats) {
+		this.seats = seats;
+	}
+
+	public String getNewSeats() {
+		return newSeats;
+	}
+
+	public void setNewSeats(String newSeats) {
+		this.newSeats = newSeats;
+	}
+
+	public String getGn() {
+		return gn;
+	}
+
+	public void setGn(String gn) {
+		this.gn = gn;
+	}
+
+	public String getBc() {
+		return bc;
+	}
+
+	public void setBc(String bc) {
+		this.bc = bc;
+	}
+
+	public String getSc() {
+		return sc;
+	}
+
+	public void setSc(String sc) {
+		this.sc = sc;
+	}
+
+	public String getSt() {
+		return st;
+	}
+
+	public void setSt(String st) {
+		this.st = st;
+	}
+
+	public String getGd() {
+		return gd;
+	}
+
+	public void setGd(String gd) {
+		this.gd = gd;
+	}
+}
